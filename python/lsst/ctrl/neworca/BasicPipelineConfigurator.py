6b6ee65e36d35def13247edf63635ef4ed1de292
srp <srp@git.lsstcorp.org>
2009-08-04 22:58:39 +0000

    Filled out some code for BasicPipelineConfigurator

--- python/lsst/ctrl/neworca/BasicPipelineConfigurator.py
+++ python/lsst/ctrl/neworca/BasicPipelineConfigurator.py
@@ -1,10 +1,11 @@
 class BasicPipelineConfigurator:
     def __init__(self):
         self.logger.log(Log.DEBUG, "BasicPipelineConfigurator:__init__")
+        self.nodes = None
 
     def configure(self):
         self.logger.log(Log.DEBUG, "BasicPipelineConfigurator:configure")
-        createNodeList()
+        self.nodes = createNodeList()
         prepPlatform()
         createLaunchScript()
         deploySetup()
@@ -13,15 +14,44 @@ class BasicPipelineConfigurator:
 
     def createNodeList(self):
         self.logger.log(Log.DEBUG, "BasicPipelineConfigurator:createNodeList")
+        node = self.policy.getArray("platform.deploy.nodes")
+
+        nodes = map(self.expandNodeHost, node)
+        # by convention, the master node is the first node in the list
+        # we use this later to launch things, so strip out the info past ":", if it's there.
+        self.masterNode = nodes[0]
+        colon = self.masterNode.find(':')
+        if colon > 1:
+            self.masterNode = self.masterNode[0:colon]
+        if orca.dryrun == False:
+            nodelist = open(os.path.join(self.dirs.get("work"), "nodelist.scr"), 'w')
+            for node in nodes:
+                print >> nodelist, node
+            nodelist.close()
+
+        return nodes
+
 
     def prepPlatform(self):
         self.logger.log(Log.DEBUG, "BasicPipelineConfigurator:prepPlatform")
+        createDirs()
 
     def deploySetup(self):
         self.logger.log(Log.DEBUG, "BasicPipelineConfigurator:deploySetup")
 
     def createDirs(self):
         self.logger.log(Log.DEBUG, "BasicPipelineConfigurator:createDirs")
+
+        dirPolicy = self.policy.getPolicy("platform.dir")
+        directories = Directories(dirPolicy, self.pipeline, self.runId)
+        self.dirs = directories.getDirs()
+
+        for name in self.dirs.names():
+            if orca.dryrun == True:
+                print "would create ",self.dirs.get(name)
+            else:
+                if not os.path.exists(self.dirs.get(name)): os.makedirs(self.dirs.get(name))
+
 
     def setupDatabase(self):
         self.logger.log(Log.DEBUG, "BasicPipelineConfigurator:setupDatabase")
@@ -44,3 +74,25 @@ class BasicPipelineConfigurator:
         #self.dbConfigurator.checkConfiguration(dbPolicy)
         #dbNames = self.dbConfigurator.prepareForNewRun(self.runid)
 
+    def expandNodeHost(self, nodeentry):
+        """Add a default network domain to a node list entry if necessary """
+
+        if nodeentry.find(".") < 0:
+            node = nodeentry
+            colon = nodeentry.find(":")
+            if colon == 0:
+                raise RuntimeError("bad nodelist format: " + nodeentry)
+            elif colon > 0:
+                node = nodeentry[0:colon]
+                if len(node) < 3:
+                    #logger.log(Log.WARN, "Suspiciously short node name: " + nod
+e)
+                    self.logger.log(Log.DEBUG, "Suspiciously short node name: " 
++ node)
+                self.logger.log(Log.DEBUG, "-> nodeentry  =" + nodeentry)
+                self.logger.log(Log.DEBUG, "-> node  =" + node)
+                node += "."+self.defaultDomain
+                nodeentry = "%s:%s" % (node, nodeentry[colon+1:])
+            else:
+                nodeentry = "%s%s:1" % (node, self.defaultDomain)
+        
