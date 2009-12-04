96a2f35a38be41e9571e87c8637a0969d26d13a1
srp <srp@git.lsstcorp.org>
2009-12-04 22:30:05 +0000

    Create a scratch directory in /tmp to hold all these things, so things are easier to find for debugging.

--- python/lsst/ctrl/orca/DagmanProductionRunConfigurator.py
+++ python/lsst/ctrl/orca/DagmanProductionRunConfigurator.py
@@ -1,7 +1,6 @@
 import os
-from lsst.ctrl.orca.ProductionRunConfigurator import ProductionRunConfigurator
-from lsst.ctrl.orca.db.DatabaseConfigurator import DatabaseConfigurator
-from lsst.ctrl.orca.provenance.Provenance import Provenance
+from lsst.ctrl.orca.BasicProductionRunConfigurator import BasicProductionRunConfigurator
+from lsst.ctrl.orca.DagConfigurator import DagConfigurator
 from lsst.pex.logging import Log
 from lsst.pex.policy import Policy
 
@@ -19,6 +18,36 @@ class DagmanProductionRunConfigurator(BasicProductionRunConfigurator):
         
         tempdir = os.path.join("/tmp", self.runid)
 
-        dagmanFile = os.path.join(self.tempdir,"dagman_"+self.runid"+.dag")
+        dagmanFile = os.path.join(tempdir,"dagman_"+self.runid+".dag")
         dagConfigurator.rewrite(dagmanTemplate, dagmanFile)
-        return dagConfigurator.totalNodeCount()
+        return dagConfigurator.getTotalNodeCount()
+
+
+    ##
+    # @brief configure this production run
+    #
+    def configure(self):
+
+        # create temporary directory for dagman-related files
+        tempdir = os.path.join("/tmp", self.runid)
+        os.mkdir(tempdir)
+
+        # grab the file name of the database before we load the policy
+        dbFileName = self.policy.getFile("databaseConfig.database").getPath()
+        dbFileName = os.path.join(self.repository, dbFileName)
+
+        # create the database
+        dbNamesDict = self.setupDatabase()
+
+        # record the database filename provenance
+
+        self.provenanceDict["user"] = self.databaseConfigurator.getUser()
+        self.provenanceDict["runid"] = self.runid
+        self.provenanceDict["dbrun"] = dbNamesDict["dbrun"]
+        self.provenanceDict["dbglobal"] = dbNamesDict["dbglobal"]
+        self.provenanceDict["repos"] = self.repository
+
+        self.provenance = self.createProvenanceRecorder(self.databaseConfigurator.getUser(), self.runid, dbNamesDict)
+
+        self.recordPolicy(dbFileName)
+        return dbNamesDict
