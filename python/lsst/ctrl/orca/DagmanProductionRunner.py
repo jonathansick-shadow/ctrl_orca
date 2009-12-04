c6f0f88861917323b6807d9619af21437a1d3595
srp <srp@git.lsstcorp.org>
2009-12-04 22:24:53 +0000

    Fix the command for the node count for the glide-in command. Fixed a typo where "-" should have been "="

--- python/lsst/ctrl/orca/DagmanProductionRunner.py
+++ python/lsst/ctrl/orca/DagmanProductionRunner.py
@@ -7,14 +7,16 @@ class DagmanProductionRunner:
         self.runid = runid
         self.nodeCount = nodeCount
         self.pipelineManagers = pipelineManagers
-        self.donotrun = True
+        self.donotrun = False
         self.verbose = True
 
     def runPipelines(self):
 
         # perform the glide-in
 
-        cmd = "condor_glidein -count "+self.nodeCount+" -setup_jobmanager=jobmanager-fork -arch=7.4.0-i686-pc-Linux-2.4 -idletime 5 grid-abe.ncsa.teragrid.org/jobmanager-pbs"
+        cmd = "condor_glidein -count %d -setup_jobmanager=jobmanager-fork -arch=7.4.0-i686-pc-Linux-2.4 -idletime 5 grid-abe.ncsa.teragrid.org/jobmanager-pbs" % self.nodeCount
+        print "running cmd = "+cmd
+
         if self.verbose == True:
             print "would run: "+cmd
         if self.donotrun == False:
@@ -27,11 +29,12 @@ class DagmanProductionRunner:
 
         # get the name of the already created "dagman_<runid>.dag" file
 
-        tmpdir - os.path.join("/tmp",self.runid)
+        tmpdir = os.path.join("/tmp",self.runid)
         dagmanFile = os.path.join(tmpdir,"dagman_"+self.runid+".dag")
 
         # launch the submit
         cmd = "condor_submit_dag "+dagmanFile
+        print "running cmd = "+cmd
 
         if self.verbose == True:
             print "would run: "+cmd
