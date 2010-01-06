4a17ea3680b757db57b85f161ced1e456a14d3a5
srp <srp@git.lsstcorp.org>
2010-01-06 20:56:03 +0000

    Don't estimate node requirements; use the node count specified in the .paf file.

--- python/lsst/ctrl/orca/DagConfigurator.py
+++ python/lsst/ctrl/orca/DagConfigurator.py
@@ -5,21 +5,13 @@ class DagConfigurator:
     def __init__(self, runid, localScratch, pipelineManagers):
         self.tmpdir = os.path.join(localScratch, runid)
         self.pipelineManagers = pipelineManagers
-        self.totalNodeCount = 0
-
     
     def helpme(self, match):
         name = match.group(1)
         print "name = "+name
-        # this counts the number of nodes when a pipeline is substituted.
-        for pipelineManager in self.pipelineManagers:
-            if pipelineManager.getPipelineName() == name:
-                self.totalNodeCount = self.totalNodeCount + pipelineManager.getNodeCount()
-                print "pipelineManager.getNodeCount = %d" % pipelineManager.getNodeCount()
         return os.path.join(self.tmpdir, name + ".condor")
 
     def rewrite(self, inFile, outFile):
-        self.totalNodeCount = 0
         input = open(inFile,"r")
         output = open(outFile,"w")
         while True:
@@ -30,7 +22,3 @@ class DagConfigurator:
             output.write(rewritten[0])
         input.close()
         output.close()
-        print "totalNodeCount after rewrite = %d" % self.totalNodeCount
-
-    def getTotalNodeCount(self):
-        return self.totalNodeCount
