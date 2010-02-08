40bfdf5354169f9c2e91f6c5c20cbda438064866
rplante <rplante@git.lsstcorp.org>
2010-02-08 17:58:01 +0000

    tests/testSharedData.py: added threaded test

--- tests/testSharedData.py
+++ tests/testSharedData.py
@@ -9,6 +9,7 @@ import os
 import sys
 import unittest
 import time
+import threading
 
 from lsst.ctrl.orca.threading import SharedData
 
@@ -173,9 +174,63 @@ class ReadableShareDataTestCase(unittest.TestCase):
             attrs = self.sd.dir()
             self.assertEquals(len(attrs), 4, "Wrong number of items: "+str(attrs))
             self.assertEquals(self.sd.lname, "Plante")
-            
 
-__all__ = "SharedDataTestCase ReadableShareDataTestCase".split()        
+class MultiThreadTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        self.sd = SharedData(False, { "c": 0 })
+
+    def tearDown(self):
+        pass
+
+    def testThreads(self):
+        t = TstThread(self.sd)
+        # pdb.set_trace()
+        self.assertEquals(self.sd.c, 0)
+        # print "c = ", self.sd.c
+
+        with self.sd:
+            t.start()
+
+            # now take a turn
+            self.sd.wait()
+            # print "c = ", self.sd.c
+            self.assertEquals(self.sd.c, 1)
+            # print "WILL NOTIFY"
+            self.sd.notifyAll()
+
+        # time.sleep(1.01)
+        with self.sd:
+            # print "WILL WAIT"
+            self.sd.wait(2.0)
+            self.assertEquals(self.sd.c, 0)
+
+        
+
+class TstThread(threading.Thread):
+
+    def __init__(self, data):
+        threading.Thread.__init__(self)
+        self.data = data
+
+    def run(self):
+        with self.data:
+            # pdb.set_trace()
+            self.data.c += 1
+            # print "++c = ", self.data.c
+
+            self.data.notifyAll()
+            # print "waiting", self.data.c
+            self.data.wait()
+
+            self.data.c -= 1
+            # print "--c = ", self.data.c
+            self.data.notifyAll()
+
+
+    
+
+__all__ = "SharedDataTestCase ReadableShareDataTestCase MultiThreadTestCase".split()        
 
 if __name__ == "__main__":
     unittest.main()
