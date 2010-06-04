898dfb4aee1fe0c55529c40373d1c9dce245594b
srp <srp@git.lsstcorp.org>
2010-06-04 21:09:59 +0000

    Add message for user, in case they don't run with full logging turned on, so they know everything has been started.

--- python/lsst/ctrl/orca/ProductionRunManager.py
+++ python/lsst/ctrl/orca/ProductionRunManager.py
@@ -184,6 +184,8 @@ class ProductionRunManager:
         if self.policy.exists("productionShutdownTopic"):
             self._startShutdownThread()
 
+        print "Production running. Waiting for events."
+
     ##
     # @brief determine whether production is currently running
     #
