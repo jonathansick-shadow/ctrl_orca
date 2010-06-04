c92a87c74e95ac4cf2b8a95c4fce14cbed793e54
srp <srp@git.lsstcorp.org>
2010-06-04 21:10:53 +0000

    Added 'C'ancel case for glidein;  removed some debug messages, and added some for users.

--- python/lsst/ctrl/orca/CondorJobs.py
+++ python/lsst/ctrl/orca/CondorJobs.py
@@ -38,7 +38,7 @@ class CondorJobs:
         num = clusterexp.findall(line)
         if len(num) == 0:
             return None
-        print "submitted job file %s.  It is job # %s" % (condorFile, num[0])
+        print "submitted job # %s as file %s" % (num[0], condorFile)
         return num[0]
     
     
@@ -50,20 +50,21 @@ class CondorJobs:
     #1016.0   srp             5/24 09:17   0+00:00:00 I  0   0.0  launch_joboffices_
     #1017.0   srp             5/24 09:18   0+00:00:00 R  0   0.0  launch_joboffices_
     
-    def waitForJobToRun(self, num):
+    def waitForJobToRun(self, num, extramsg=None):
         self.logger.log(Log.DEBUG, "CondorJobs:waitForJobToRun")
         jobNum = "%s.0" % num
         queueExp = re.compile("\S+")
         cJobSeen = 0
         print "waiting for job %s to run." % num
-        print "this might take a few minutes"
+        if extramsg != None:
+            print extramsg
         secondsWaited = 0
         while 1:
             pop = os.popen("condor_q", "r")
             bJobSeenNow = False
             if (secondsWaited > 0) and ((secondsWaited % 60) == 0):
                 minutes = secondsWaited/60
-                print "Waited %d minute%s so far. Still waiting for job %s to run." % ((secondsWaited / 60), ("" if (minutes == 1) else "s"), num)
+                print "waited %d minute%s so far. still waiting for job %s to run." % ((secondsWaited / 60), ("" if (minutes == 1) else "s"), num)
             while 1:
                 line = pop.readline()
                 if not line:
@@ -76,21 +77,25 @@ class CondorJobs:
                     cJobSeen = cJobSeen + 1
                     bJobSeenNow = True
                 if (values[0] == jobNum) and (runstate == 'R'):
-                    print values
                     pop.close()
                     print "Job %s is now being run." % num
-                    return 'R'
+                    return runstate
                 if (values[0] == jobNum) and (runstate == 'H'):
                     pop.close()
                     # throw exception here
                     print "Job %s is being held.  Please review the logs." % num
-                    return 'H'
+                    return runstate
                 if (values[0] == jobNum) and (runstate == 'X'):
                     print values
                     pop.close()
                     # throw exception here
                     print "Saw job %s, but it was being aborted" % num
-                    return 'X'
+                    return runstate
+                if (values[0] == jobNum) and (runstate == 'C'):
+                    pop.close()
+                    # throw exception here
+                    print "Job %s is being cancelled." % num
+                    return runstate
             # check to see if we've seen the job before, but that
             # it disappeared
             if (cJobSeen > 0) and (bJobSeenNow == False):
