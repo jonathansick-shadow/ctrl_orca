14c05c9e700150af51c309d70905e1958b8cae27
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2012-04-17 16:12:37 -0500

    New version of the data file

--- python/lsst/ctrl/orca/config/production_data.py
+++ python/lsst/ctrl/orca/config/production_data.py
@@ -3,22 +3,28 @@ root.production.eventBrokerHost = "lsst8.ncsa.uiuc.edu"
 root.production.repositoryDirectory = "./pipeline"
 root.production.productionShutdownTopic = "productionShutdown"
 
-root.database.name = "dc3bGlobal"
-root.database.system.authInfo.host = "fester.ncsa.uiuc.edu"
-root.database.system.authInfo.port = 3306
-root.database.system.runCleanup.daysFirstNotice = 7
-root.database.system.runCleanup.daysFinalNotice = 1
+root.databaseConfigNames = ["db1"]
+root.database["db1"].name = "dc3bGlobal"
+root.database["db1"].system.authInfo.host = "fester.ncsa.uiuc.edu"
+root.database["db1"].system.authInfo.port = 3306
+root.database["db1"].system.runCleanup.daysFirstNotice = 7
+root.database["db1"].system.runCleanup.daysFinalNotice = 1
 
-root.database.configurationClass = "lsst.ctrl.orca.db.DC3Configurator"
-root.database.configurationDictionary = "databaseDictionary.py"
-root.database.logger.launch = True
+root.database["db1"].configurationClass = "lsst.ctrl.orca.db.DC3Configurator"
+root.database["db1"].configuration.globalDbName = "GlobalDB"
+root.database["db1"].configuration.dcVersion = "PT1_2"
+root.database["db1"].configuration.dcDbName = "DC3b_DB"
+root.database["db1"].configuration.minPercDiskSpaceReq = 10
+root.database["db1"].configuration.userRunLife = 2
+root.database["db1"].logger.launch = True
 
 root.workflowNames = ["PT1Workflow"]
 
 root.workflow["PT1Workflow"].platform = "@platform/lonestar.paf"
 root.workflow["PT1Workflow"].shutdownTopic = "workflowShutdown"
 
-root.workflow["PT1Workflow"].configurationClass = "lsst.ctrl.VanillaCondorWorkflowConfigurator"
+root.workflow["PT1Workflow"].configurationClass = "lsst.ctrl.orca.VanillaCondorWorkflowConfigurator"
+root.workflow["PT1Workflow"].configuration["vanilla"].deployData.dataRepository = "/scratch/00342/ux453102/base/lsst/pt1_2-410/data/obs"
 root.workflow["PT1Workflow"].configuration["vanilla"].deployData.collection = "ImSim"
 root.workflow["PT1Workflow"].configuration["vanilla"].deployData.script = "$DATAREL_DIR/bin/runOrca/deployData-abe.sh"
 
@@ -28,7 +34,7 @@ root.workflow["PT1Workflow"].configuration["vanilla"].condorData.ftpNode = "grid
 root.workflow["PT1Workflow"].configuration["vanilla"].condorData.transferProtocol = "gsiftp"
 root.workflow["PT1Workflow"].configuration["vanilla"].glideinRequest.keyNames = ["CONDOR_SBIN", "CPU_COUNT", "MACHINE_COUNT", "QUEUE", "MAX_WALLTIME", "PROJECT"]
 root.workflow["PT1Workflow"].configuration["vanilla"].glideinRequest.keyValuePairs["CONDOR_SBIN"] = "/share/apps/teragrid/condor-7.4.4.-r1/sbin"
-root.workflow["PT1Workflow"].configuration["vanilla"].glideinRequest.keyValuePairs["CPU_COUNT"] = "6"
+root.workflow["PT1Workflow"].configuration["vanilla"].glideinRequest.keyValuePairs["CPU_COUNT"] = "6" 
 root.workflow["PT1Workflow"].configuration["vanilla"].glideinRequest.keyValuePairs["MACHINE_COUNT"] = "64"
 root.workflow["PT1Workflow"].configuration["vanilla"].glideinRequest.keyValuePairs["QUEUE"] = "normal"
 root.workflow["PT1Workflow"].configuration["vanilla"].glideinRequest.keyValuePairs["MAX_WALLTIME"] = "180"
@@ -38,6 +44,9 @@ root.workflow["PT1Workflow"].configuration["vanilla"].glideinRequest.outputFileN
 root.workflow["PT1Workflow"].configuration["vanilla"].announceData.script = "$CTRL_SCHED_DIR/bin/announceDataset.py"
 root.workflow["PT1Workflow"].configuration["vanilla"].announceData.topic = "RawCcdAvailable"
 root.workflow["PT1Workflow"].configuration["vanilla"].announceData.inputdata = "/nfs/lsst3/remote/Wint-2012/pipeline/miniprod.input"
+root.workflow["PT1Workflow"].configuration["vanilla"].announceData.dataCompleted.script = "$CTRL_ORCA_DIR/bin/sendStatus.py"
+root.workflow["PT1Workflow"].configuration["vanilla"].announceData.dataCompleted.topic = "JobOfficeTopic"
+root.workflow["PT1Workflow"].configuration["vanilla"].announceData.dataCompleted.status = "data:completed"
 
 
 
